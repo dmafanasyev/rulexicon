183102e97fde7790e053ebf0d01ed8171dc4cc2b
dmafanasyev <dmafanasyev@gmail.com>
2020-03-28 22:41:56 +0300

    Citation update

---
+++ R/rulexicon-package.R
@@ -0,0 +1,7 @@
+#' Russian Lexicons for Text Analysis
+#'
+#' An R data package containing a collection of Russian lexicons, polarity tables, words lists, lemmatization tables and etc. for text analysis.
+#' @docType package
+#' @name rulexicon
+#' @aliases rulexicon package-rulexicon
+NULL
